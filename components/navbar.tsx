83817c4ece7269870e051c2de1e28194817fe45d
Sonu Kumar <sk2255fh@gmail.com>
2026-04-14 21:44:04 +0530

    fix-btn

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -66,7 +66,7 @@ export function Navbar() {
                                             {item.label}
                                         </Link>
                                     ))}
-                                    <Link href="tel:1234567890" className="px-5 py-2 rounded-md bg-orange-600 hover:bg-orange-700">
+                                    <Link href="tel:1234567890" className="px-5 text-center py-2 rounded-md bg-orange-600 hover:bg-orange-700">
                                         Order Now
                                     </Link>
                                 </div>
